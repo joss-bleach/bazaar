127195ee91a9487851fe950fb381fdc0d41abadd
Joss Bleach <jossbleach@josss-mbp.home>
2024-05-22 15:02:48 +0100

    filtering working

--- app/(root)/_components/sidebar/category-filter.tsx
+++ app/(root)/_components/sidebar/category-filter.tsx
@@ -1,27 +1,75 @@
-import { CarFront, Phone, Shirt } from "lucide-react";
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
+import { CarFront, Medal, Phone, Shirt } from "lucide-react";
+
+// Utils
+import { addQueryToUrl, RemoveQueryFromUrl } from "@/lib/utils";
 
 // Components
 import { Button } from "@/components/ui/button";
 
+const popularCategories = [
+  {
+    label: "Clothing",
+    id: "664df8639c1f418dd5c1038b",
+    icon: Shirt,
+  },
+  {
+    label: "Electronics",
+    id: "6648ba12dee0fe79a4e46750",
+    icon: Phone,
+  },
+  {
+    label: "Vehicles",
+    id: "664df86a9c1f418dd5c1038d",
+    icon: CarFront,
+  },
+  {
+    label: "Collectables",
+    id: "664df95b9c1f418dd5c10391",
+    icon: Medal,
+  },
+];
+
 export const CategoryFilter = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const handleOnSelectCategory = (categoryId: string) => {
+    let newUrl = "";
+    if (!searchParams.get("category")) {
+      newUrl = addQueryToUrl({
+        params: searchParams.toString(),
+        key: "category",
+        value: categoryId,
+      });
+    } else {
+      newUrl = RemoveQueryFromUrl({
+        params: searchParams.toString(),
+        keysToRemove: ["category"],
+      });
+    }
+
+    router.push(newUrl, { scroll: false });
+  };
   return (
     <div className="flex flex-col gap-2 w-full">
       <h2 className="text-sm font-semibold tracking-wide uppercase">
         Popular Categories
       </h2>
       <div className="w-full">
-        <Button
-          className="flex w-full justify-start items-center"
-          variant="ghost"
-        >
-          <Shirt size={14} className="mr-2" /> Clothing
-        </Button>
-        <Button className="flex w-full justify-start" variant="ghost">
-          <Phone size={14} className="mr-2" /> Electronics
-        </Button>
-        <Button className="flex w-full justify-start" variant="ghost">
-          <CarFront size={14} className="mr-2" /> Vehicles
-        </Button>
+        {popularCategories.map(({ label, id, icon: Icon }) => {
+          return (
+            <Button
+              key={id}
+              className="flex w-full justify-start items-center"
+              variant="ghost"
+              onClick={() => handleOnSelectCategory(id)}
+            >
+              <Icon size={14} className="mr-2" /> {label}
+            </Button>
+          );
+        })}
       </div>
     </div>
   );
--- app/(root)/_components/sidebar/price-filter.tsx
+++ app/(root)/_components/sidebar/price-filter.tsx
@@ -1,9 +1,68 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+// Utils
+import { addQueryToUrl, RemoveQueryFromUrl } from "@/lib/utils";
+
 import { PoundSterling } from "lucide-react";
 
 // Components
 import { Input } from "@/components/ui/input";
 
 export const PriceFilter = () => {
+  const [minPrice, setMinPrice] = useState<string>("");
+  const [maxPrice, setMaxPrice] = useState<string>("");
+
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const debounce = setTimeout(() => {
+      let newUrl = "";
+
+      if (minPrice) {
+        newUrl = addQueryToUrl({
+          params: searchParams.toString(),
+          key: "min",
+          value: minPrice,
+        });
+      } else {
+        newUrl = RemoveQueryFromUrl({
+          params: searchParams.toString(),
+          keysToRemove: ["min"],
+        });
+      }
+
+      router.push(newUrl, { scroll: false });
+    }, 300);
+
+    return () => clearTimeout(debounce);
+  }, [minPrice, searchParams, router]);
+
+  useEffect(() => {
+    const debounce = setTimeout(() => {
+      let newUrl = "";
+
+      if (maxPrice) {
+        newUrl = addQueryToUrl({
+          params: searchParams.toString(),
+          key: "max",
+          value: maxPrice,
+        });
+      } else {
+        newUrl = RemoveQueryFromUrl({
+          params: searchParams.toString(),
+          keysToRemove: ["max"],
+        });
+      }
+
+      router.push(newUrl, { scroll: false });
+    }, 300);
+
+    return () => clearTimeout(debounce);
+  }, [maxPrice, searchParams, router]);
+
   return (
     <div className="flex flex-col gap-2">
       <h2 className="text-sm font-semibold tracking-wide uppercase">
@@ -15,14 +74,24 @@ export const PriceFilter = () => {
             className="text-muted-foreground absolute left-2"
             size={16}
           />
-          <Input placeholder="min" className="pl-6" type="number" />
+          <Input
+            onChange={(e) => setMinPrice(e.target.value)}
+            placeholder="min"
+            className="pl-6"
+            type="number"
+          />
         </div>
         <div className="relative flex items-center">
           <PoundSterling
             className="text-muted-foreground absolute left-2"
             size={16}
           />
-          <Input placeholder="max" className="pl-6" type="number" />
+          <Input
+            onChange={(e) => setMaxPrice(e.target.value)}
+            placeholder="max"
+            className="pl-6"
+            type="number"
+          />
         </div>
       </div>
     </div>
